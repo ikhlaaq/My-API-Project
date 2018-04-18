1b6dfe164b7622dee9d92a965283456f15b46a97
ikhlaaq <ikhlaak@hotmail.com>
2018-04-19 00:41:16 +0200

    API Project

---
+++ app.js
@@ -0,0 +1,28 @@
+$(function () {
+
+    //navigation
+
+    var active1 = false;
+    var active2 = false;
+    var active3 = false;
+    var active4 = false;
+
+    $('.parent').on('mousedown touchstart', function () {
+
+        if (!active1) $(this).find('.contact').css({ 'transform': 'translate(0px,125px)' });
+        else $(this).find('.contact').css({ 'transform': 'none' });
+        if (!active2) $(this).find('.itunes').css({ 'transform': 'translate(60px,105px)' });
+        else $(this).find('.itunes').css({ 'transform': 'none' });
+        if (!active3) $(this).find('.movies').css({ 'transform': 'translate(105px,60px)' });
+        else $(this).find('.movies').css({ 'transform': 'none' });
+        if (!active4) $(this).find('.books').css({ 'transform': 'translate(125px,0px)' });
+        else $(this).find('.books').css({ 'transform': 'none' });
+        active1 = !active1;
+        active2 = !active2;
+        active3 = !active3;
+        active4 = !active4;
+
+    });
+
+
+});
